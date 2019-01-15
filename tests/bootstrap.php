8f7770065256b474f8a5b21cf635664e1672a197
Petr Morávek <petr@pada.cz>
2019-01-15 11:08:10 +0100

    tests: moving tests to top-level dir

---
+++ tests/bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+declare(strict_types = 1);
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+if (getenv('IS_PHPSTAN') !== '1') {
+	// configure environment
+	date_default_timezone_set('Europe/Prague');
+	umask(0);
+	Tester\Environment::setup();
+}
+
+// create temporary directory
+define('TEMP_DIR', __DIR__ . '/tmp/' . (isset($_SERVER['argv']) ? md5(serialize($_SERVER['argv'])) : getmypid()));
+if (getenv('IS_PHPSTAN') !== '1') {
+	Tester\Helpers::purge(TEMP_DIR);
+	Tracy\Debugger::$logDirectory = TEMP_DIR;
+}
+
+$_ENV = $_GET = $_POST = $_FILES = [];
